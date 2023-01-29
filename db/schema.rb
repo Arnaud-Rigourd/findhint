c2895506611f2a4f4b4b42ac1efc100ccd0cadaf
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-29 14:03:07 +0100

    Front in progress on first challenge. Change content answer to string instead of text

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_122138) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_125931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
     t.bigint "enigma_id", null: false
+    t.string "content"
     t.index ["enigma_id"], name: "index_answers_on_enigma_id"
   end
 

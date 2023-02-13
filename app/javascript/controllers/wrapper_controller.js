fb3e03994eb33f87182e0486f3d83b890d9632f8
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-02-13 13:09:28 +0100

    Add reveal on final enigma

--- app/javascript/controllers/wrapper_controller.js
+++ app/javascript/controllers/wrapper_controller.js
@@ -2,18 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="wrapper"
 export default class extends Controller {
-  static targets = ["scroll", "scrollItems", "comingSoon"]
+  static targets = ["scroll", "siteInfo", "contactMessage"]
 
   connect() {
   }
 
   hide(){
-    if (this.scrollTarget.getBoundingClientRect()['y'] < window.innerHeight - 60) {
-      this.scrollTarget.style = "opacity: 0"
-    } else {
-      this.scrollTarget.style = "opacity: 1"
-    }
+    this.scrollTargets.forEach((s) => {
+      if (s.getBoundingClientRect()['y'] < window.innerHeight - 60) {
+        s.style = "opacity: 0"
+      } else {
+        s.style = "opacity: 1"
+      }
+    })
   }
 
+  reveal() {
+    if (!this.contactMessageTarget.classList.contains('hidden')) {
+      this.siteInfoTarget.classList.remove('hidden')
+      this.scrollTargets['1'].classList.remove('hidden')
 
+      if (this.scrollTargets['1'].getBoundingClientRect()['y'] < window.innerHeight - 60) {
+        this.scrollTargets['1'].style = "opacity: 0"
+      } else {
+        this.scrollTargets['1'].style = "opacity: 1"
+      }
+    }
+  }
 }

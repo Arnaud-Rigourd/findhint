cc5ebf88e1fa91c1970992d838e605d194df3cf8
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-02-14 11:23:39 +0100

    revealdone

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -4,9 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["yesBtn", "yesLink", "noBtn", "loadingContainer", "title", "mainContainer", "yesNoDiv"]
 
-  connect() {
-  }
-
   loading() {
     this.paragraphes = document.querySelectorAll('p')
     this.darkContrast = "#291f1f5d"
